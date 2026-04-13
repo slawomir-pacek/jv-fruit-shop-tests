a2264d75ec2b6d4833df1a9f96c17f62c39d68ae
Slawomir Pacek <slawomir_pacek@op.pl>
2026-04-13 11:58:27 +0200

    fruitShopTest10

--- src/main/java/core/basesyntax/service/impl/ShopServiceTest.java
+++ src/main/java/core/basesyntax/service/impl/ShopServiceTest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ShopServiceTest {

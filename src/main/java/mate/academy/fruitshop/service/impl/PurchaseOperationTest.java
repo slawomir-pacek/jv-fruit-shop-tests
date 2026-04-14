4f878744ad9738e4b5f6e2ec1e88d0db5f08bfd3
Slawomir Pacek <slawomir_pacek@op.pl>
2026-04-14 11:46:39 +0200

    fruitShopTest18

--- src/main/java/mate/academy/fruitshop/service/impl/PurchaseOperationTest.java
+++ src/main/java/mate/academy/fruitshop/service/impl/PurchaseOperationTest.java
@@ -2,10 +2,10 @@ package mate.academy.fruitshop.service.impl;
 
 import java.util.HashMap;
 import java.util.Map;
+import mate.academy.fruitshop.model.FruitTransaction;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import mate.academy.fruitshop.model.FruitTransaction;
 
 class PurchaseOperationTest {
 

0b38647f32b818d076066a6892066d63805a5d6e
Slawomir Pacek <slawomir_pacek@op.pl>
2026-04-14 12:29:24 +0200

    fruitShopTest29

--- src/main/java/mate/academy/fruitshop/service/impl/PurchaseOperationTest.java
+++ src/main/java/mate/academy/fruitshop/service/impl/PurchaseOperationTest.java
@@ -1,11 +1,9 @@
 package mate.academy.fruitshop.service.impl;
 
-import mate.academy.fruitshop.model.FruitTransaction;
-import org.junit.jupiter.api.Test;
-
 import java.util.HashMap;
 import java.util.Map;
-
+import mate.academy.fruitshop.model.FruitTransaction;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
--- src/main/java/mate/academy/fruitshop/service/impl/ReturnOperationTest.java
+++ src/main/java/mate/academy/fruitshop/service/impl/ReturnOperationTest.java
@@ -3,8 +3,8 @@ package mate.academy.fruitshop.service.impl;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import mate.academy.fruitshop.model.FruitTransaction;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReturnOperationTest {
 
